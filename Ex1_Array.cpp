d1487fc236c567feacf3a0955040ca7b83c66519
Bonvjppro <trietminhpham79@gmail.com>
2023-10-22 21:03:31 +0700

    them max min

--- Ex1_Array.cpp
+++ Ex1_Array.cpp
@@ -6,20 +6,36 @@ int main() {
     printf("Nhap so phan tu: ");
     scanf("%d", &n);
     int numbers[n];
-    for (int i = 0; i < n; i++){
+    for (int i = 1; i <= n; i++){
+        printf("Phan tu thu %d: ", i);
         scanf("%d", &numbers[i]);
     }
     int count = 0;
-    for(int i = 0; i < n; i++){
+    for(int i = 1; i < n; i++){
          if(i % 2 == 0 && i > 0 ){
             if(numbers[i] % 2 != 0){
                sum += numbers[i];
                count++;
-               printf("vi tri: %d \n",i);
+               printf("vi tri thu: %d \n",i);
             }
         } 
     } 
     double avg = (double)sum / count;
     printf("trung binh cong cac so le o vi tri chan la: %.2lf", avg);
+    int biggestNumbers = numbers[1];
+    for (int i = 1; i <= n; i++){
+        if(numbers[i] > biggestNumbers){
+            biggestNumbers = numbers[i];
+        }
+    }
+    printf("\nSo lon nhat trong mang la: %d", biggestNumbers);
+    int smallestNumbers = numbers[1];
+    for(int i=1; i <= n; i++){
+        if(numbers[i] < smallestNumbers){
+            smallestNumbers = numbers[i];
+        }
+    }
+    printf("\nSo nho nhat trong mang la: %d", smallestNumbers);
+    
     return 0;
 }
